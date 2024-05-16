4ecc60d297985af3ccf626a18e7123134a6d8270
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-05-16 15:55:28 +0100

    Developing styling of web page

--- mysite/crimes/views.py
+++ mysite/crimes/views.py
@@ -18,25 +18,23 @@ def home(request):
 
 
 def postcode_page(request, postcode):
-    '''
-    crime_df = get_crime_data_df("%s" % postcode, 2023, 1)
-    street_data = counting_by_category(crime_df, ["street name"])
-    '''
 
     try:
         crime_df = get_crime_data_df(postcode, 2023, 1)
     except:
         return HttpResponseNotFound("404 Error: Invalid Postcode")
 
+    # Category Bar Chart
     crime_category_df = counting_by_category(crime_df, ['category'])
     plot_bar(crime_category_df, f"crimes/static/png/bar_chart")
 
+    # Streets Table
     crime_street_df = crime_df.groupby('street')['category'].agg(
         lambda x: x.mode().iloc[0] if not x.empty else None).reset_index()
     crime_street_df['total_crimes'] = crime_df.groupby(
         'street').size().reset_index(name='total_crimes')['total_crimes']
 
-    context = {"postcode": postcode.upper(),
+    context = {"postcode": postcode[:-3].strip().upper() + ' ' + postcode[-3:].strip().upper(),
                "crime_street_df": crime_street_df.sort_values('total_crimes', ascending=False).head(10).iterrows()}
 
     return render(request, "crimes/postcode_page.html", context)
