d731709bd92c4cb696d56f8cfa802826a4c9f2aa
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-05-17 14:51:25 +0100

    Added function for line graphs

--- mysite/crimes/data/visualise.py
+++ mysite/crimes/data/visualise.py
@@ -25,28 +25,39 @@ def plot_bar(df: pd.core.frame.DataFrame, file_path: str) -> None:
     plt.savefig(file_path, bbox_inches='tight')
 
 
-def plot_line(df: pd.core.frame.DataFrame, file_path: str) -> None:
-    """This function produces a bar chart based on the inputted dataframe and category for
-    the bars. The function saves the image to the inputted filepath."""
+def plot_crimes_with_time_line_graph(df: pd.core.frame.DataFrame, file_path: str) -> None:
+    """This function produces a line graph based on the inputted dataframe
+    with two columns: date and total number of crimes (the inputted dataframe may 
+    be filtered in some way - e.g. specific street(s) or category of crime).
+    The function saves an image of the graph to the inputted filepath."""
 
     font_dict = {'weight': 'bold', 'size': 14}
 
     use('agg')
     sns.set_theme(style='darkgrid', palette='deep', font='monospace')
     plt.figure(facecolor='lightgray')
+    sns.lineplot(x=df.index, y=df[df.columns[0]],
+                 marker='.', markersize=10, markerfacecolor='gray')
+    plt.xlabel('Date (Month-Year)', fontdict=font_dict)
+    plt.xticks(ticks=df.index, labels=[
+               date.strftime("%m-%Y") for date in df.index], rotation=305)
+    plt.ylabel('Number of Crimes', fontdict=font_dict)
+    plt.title(
+        f'Number of Crimes by {df.columns[0].title()}', fontdict=font_dict)
+    plt.savefig(file_path, bbox_inches='tight')
 
-    pass
 
-
+'''
 if __name__ == "__main__":
 
     # crime_df = get_crime_data_df('NW5 1TU', 2023, 1)
 
     # crime_cat_df = counting_by_category(crime_df, ['outcome'])
 
-    all_crime_df = get_all_crime_data_df('nw5 1tu')
-    all_crime_cat_df = counting_by_category(all_crime_df, ['category'])
+    all_crime_df = get_crime_data_df('nw5 1tu', 2023)
+    all_crime_date_df = counting_by_category(all_crime_df, ['date'])
 
-    file_path = "static/png/category_chart"
+    print(all_crime_date_df)
 
-    plot_bar(all_crime_cat_df, file_path)
+    plot_crimes_with_time_line_graph(all_crime_date_df, 'yo')
+'''
